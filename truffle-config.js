706d3aea61b65d3cff986097a0f408b86aabad97
raniahal <raniahalila777@gmail.com>
2025-03-29 16:03:18 +0100

    new local config

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,16 @@
+let localConfig = {};
+try {
+  localConfig = require("./truffle-config.local");
+} catch (err) {
+  console.log("No local config found, using default settings.");
+}
+
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost
-      port: 7545,            // Port par défaut de Ganache
-      network_id: "*",       // N'importe quel network id
-    },
-  },
-
-  compilers: {
-    solc: {
-      version: "0.8.17",     // Version Solidity stable
-    },
-  },
+      host: "127.0.0.1",
+      port: localConfig.port || 7545, // Default: 7545
+      network_id: localConfig.network_id || "*", // Default: any network
+    }
+  }
 };
